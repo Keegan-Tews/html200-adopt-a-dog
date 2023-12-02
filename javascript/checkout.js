b5671aa928d9cde62aad34a8f532afa79224a810
Keegan Tews <keegan.tews@yahoo.com>
2023-12-02 14:53:10 -0800

    added jQuery to the site

--- javascript/checkout.js
+++ javascript/checkout.js
@@ -1,4 +1,4 @@
-const form = document.querySelector('form');
+/*const form = document.querySelector('form');
 
 // Add an event listener to the form's submit 
 form.addEventListener('submit', (e) => {
@@ -16,4 +16,20 @@ form.addEventListener('submit', (e) => {
 
   // Display an alert message
   alert('Thank you! The form information has been received.');
+});
+*/
+
+$(document).ready(function() {
+  $('form').submit(function(e) {
+    e.preventDefault();
+
+    // Serialize the form data
+    const formData = $(this).serialize();
+
+    // Log the form data to the console
+    console.log(formData);
+
+    // Display an alert message
+    alert('Thank you! The form information has been received.');
+  });
 });
--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,4 @@
-
-
 let total = 0;
-
-
-
-
-
 
 function dogImg(dog) {
     
@@ -16,8 +9,7 @@ function dogImg(dog) {
     alert("This is " + name1 + " the " + breed1 + " and his fee is $" + fee1 + ".");
 }
 
-
-function runningTotal(t) {
+/* function runningTotal(t) {
     let cart = document.getElementsByClassName("total-cart");
     let dataCart = cart[0].getAttribute("data-total");
     let carthtml = cart[0].innerHTML;
@@ -30,57 +22,44 @@ function runningTotal(t) {
     alert(total);
 
 }
+*/
 
+$(document).ready(function() {
+  $('button').click(function() {
+    let cart = $('.total-cart');
+    let dataCart = cart.data('total');
+    let carthtml = cart.html();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*/ Get all the dog photo elements
-const dogPhotos = document.querySelectorAll('.dog-pic');
-
-// Add click event listener to each dog photo
-dogPhotos.forEach((dogPhoto) => {
-  dogPhoto.addEventListener('click', function() {
-    const dogName = this.nextElementSibling.querySelector('h3').textContent;
-    const fee = parseFloat(this.nextElementSibling.querySelector('h4').textContent.split('$')[1]);
-
-    alert("Name: " + dogName + "\nAdoption Fee: $" + fee.toFixed(2));
+    let fee = $(this).data('fee');
+    let total = parseFloat(dataCart) + parseFloat(fee);
+    cart.data('total', total);
+    total = "$" + total;
+    cart.html(total);
+    alert(total);
   });
 });
 
 
+$(document).ready(function() {
+  $('.square').hover(function() {
+    $(this).addClass('shadow-box');
+    $('.shadow-box').css('border', '1px solid black');
+    $('.shadow-box').css('border-radius', '5px');
+    $('.shadow-box').css('padding', '5px');
+    $('.shadow-box').css('margin', '5px');
+    $('.shadow-box').css('background-color', 'white');
+  }, 
+   function() {
+    $(this).removeClass('shadow-box');
+    $('.square').css('border', 'none');
+    $('.square').css('border-radius', 'none');
+    $('.square').css('padding', 'none');
+    $('.square').css('margin', 'none');
+    $('.square').css('background-color', 'none');
+    $('.square').css('box-shadow', 'none');
+  });
 
-
-
-
-
-function totalFee() {
-
-}*/
+  $('.square button').click(function() {
+    $(this).css('background-color', 'red');
+  });
+});
